0e21423f93ffe3c8ff2a3afaa9fe48a06a11bdcf
rahulanand16nov <rahulanand16nov@gmail.com>
2020-01-03 23:57:34 +0530

    Getting JSON for subreddits

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"getReddit/http"
+	"getReddit/reddit"
 )
 
-func main(){
+func main() {
 	fmt.Println("getReddit - v0.1")
+	mainListing := reddit.Listing{}
+	http.GetJSON("https://www.reddit.com/r/askReddit/.json", mainListing)
+	fmt.Println("Title: %V", mainListing.Data.Childrens[0])
 }
--- reddit/reddit.go
+++ reddit/reddit.go
@@ -1,16 +1,21 @@
 package reddit
 
-import (
-	"getReddit/http"
-	
-)
+import ()
+
+// This format will be extracted from the heavy JSON we get from the API
 
 type Post struct {
 	Data struct {
-	  Domain string `json: "domain"`
-	  Subreddit string `json: "subreddit"`
-	  Title string `json: "title"`
-	  Permalink string `json: "permalink"`
-	  Url string `json: "url"`
+		Subreddit string `json: "subreddit"`
+		Title     string `json: "title"`
+		Author    string `json: "author"`
+		Permalink string `json: "permalink"`
+		Url       string `json: "url"`
 	} `json: "data"`
+}
+
+type Listing struct {
+	Data struct {
+		Childrens []Post `json: "children"`
+	} `json : "data"`
 }
